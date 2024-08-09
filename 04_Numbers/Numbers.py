b85fc624cd71fbe7720d75da672820e0c09e22fb
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-08-09 10:45:40 +0000

    Numbers

---
+++ 04_Numbers/Numbers.py
@@ -0,0 +1,34 @@
+x=2
+y=3
+z=4
+
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x**y)
+# print(x%y)
+
+# print(x+y*z) # Incorrect format
+# 14
+
+# print(x+(y*z)) , Correct format
+# 14
+
+# Operator Overloading in python
+# print('Chai'+'code') 
+# Outputs: Chaicode
+
+w = x,y,z # when no. are called like this they return a tuple.
+# print(w) 
+# Outputs: (2,3,4)
+
+w = x+1,y+3,z-1
+# print(w) 
+# Outputs: (3,6,3)
+
+# print(2**1000)
+#10715086071862673209484250490600018105614048117055336074437503883703510511249361224931983788156958581275946729175531468251871452856923140435984577574698574803934567774824230985421074605062371141877954182153046474983581941267398767559165543946077062914571196477686542167660429831652624386837205668069376
+
+repr('Chai')
+str('Chai')
+print('Chai')
